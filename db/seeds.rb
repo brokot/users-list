370848a182f445eab17b2d00ea1579d60b0d299a
Constantin Brovkov <brovkovkot@gmail.com>
2021-02-22 14:22:46 +0300

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10000.times.each do
+  loop do
+    name = "#{Faker::Name.first_name} #{Faker::Name.last_name}"
+    user = User.new(
+        email: Faker::Internet.email(name: name),
+        name: name,
+        title: Faker::Lorem.sentence,
+        phone: Faker::PhoneNumber.cell_phone_in_e164,
+        status: %w(active inactive).sample
+    )
+    break if user.save
+  end
+end
