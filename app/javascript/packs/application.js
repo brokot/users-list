90eaa9642ce9f3574fa627ad19f57c11599dd99f
Constantin Brovkov <brovkovkot@gmail.com>
2021-02-22 13:00:37 +0300

    Change application pack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,21 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import ReactOnRails from 'react-on-rails';
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+import UsersApp from '../application/loadables/users_loadable.jsx';
+
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
+
+ReactOnRails.register({
+  UsersApp,
+});
+
+// Styles
+import '@shopify/polaris/dist/styles.css';
