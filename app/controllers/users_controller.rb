a3ac775aa8bb63d37fafb90afe31058651dc096e
Constantin Brovkov <brovkovkot@gmail.com>
2021-02-23 15:24:24 +0300

    UsersController#index: Paginate users list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,11 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    @users = User.all
-    render json: { users: @users }, status: :ok
+    @users = User.page(params[:page]).per(params[:limit])
+    render json: {
+        users: @users,
+        meta: pagination_meta(@users)
+    }, status: :ok
   end
 
   # GET /users/1 or /users/1.json
