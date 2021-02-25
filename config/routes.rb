c72e72714d280e2454d229cfe72da0bd136c55d1
Constantin Brovkov <brovkovkot@gmail.com>
2021-02-25 19:38:51 +0300

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  resources :users, only: %i[index show create update delete]
+  resources :users, only: %i[index show create update destroy]
 end
