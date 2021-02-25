b433cba8769e541e2a7881da7cccd16afe994a74
Constantin Brovkov <brovkovkot@gmail.com>
2021-02-25 19:07:08 +0300

    Added actions to load and set user

--- app/javascript/application/actions/users.js
+++ app/javascript/application/actions/users.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 export const SET_LOADING = 'SET_LOADING';
+export const SET_SAVING = 'SET_SAVING';
+export const SET_USER = 'SET_USER';
+export const SET_USER_ATTRIBUTE = 'SET_USER_ATTRIBUTE';
 export const SET_USERS = 'SET_USERS';
 
 export function onSetLoading(loading) {
@@ -10,11 +13,49 @@ export function onSetLoading(loading) {
   };
 }
 
+export function onSetSaving(saving) {
+  return {
+    type: SET_SAVING,
+    saving,
+  };
+}
+
+export function onSetUser(user) {
+  return {
+    type: SET_USER,
+    user,
+  };
+}
+
+export function onSetUserAttribute(key, value) {
+  return {
+    type: SET_USER_ATTRIBUTE,
+    key,
+    value,
+  };
+}
+
 export function onSetUsers(users) {
   return {
     type: SET_USERS,
     users,
   };
+}
+
+export function loadUser(id) {
+  return (dispatch) => {
+    dispatch(onSetSaving(true));
+    dispatch(onSetUser({ id }));
+    axios.get(`/users/${id}`).then((response) => {
+      const { data } = response;
+      dispatch(onSetUser(data.user));
+      dispatch(onSetSaving(false));
+    }).catch((error) => {
+      dispatch(onSetUser(null));
+      dispatch(onSetSaving(false));
+      console.log(error);
+    });
+  }
 }
 
 export function loadUsers(params, success) {
