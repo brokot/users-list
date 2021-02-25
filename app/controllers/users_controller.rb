12a813c7e4351094e80914fe99e1ec410afadfdd
Constantin Brovkov <brovkovkot@gmail.com>
2021-02-25 18:52:01 +0300

    Change UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,42 +12,26 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
-  end
-
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
-  # GET /users/1/edit
-  def edit
+    render json: { user: @user }, status: :ok
   end
 
   # POST /users or /users.json
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      render json: { user: @user }, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /users/1 or /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      render json: { user: @user }, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
     end
   end
 
