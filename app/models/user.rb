9a8e959c0cb6476ffe8e5482bbf58a96ac0d031b
Constantin Brovkov <brovkovkot@gmail.com>
2021-02-25 20:06:06 +0300

    User: add email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :name, :email, :phone, presence: true
   validates :email, uniqueness: true
+  validates_format_of :email, with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 
   enum status: [:active, :inactive]
 end
